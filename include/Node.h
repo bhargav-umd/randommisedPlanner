9ed17186913b62dc171ca52f2fd0db89bd82750a
bhargav-umd <bhargav@umd.edu>
2019-04-03 03:29:01 -0400

    added comments for headers

--- include/Node.h
+++ include/Node.h
@@ -1,11 +1,47 @@
+/* Copyright (C)
+ * 2019 - Bhargav Dandamudi
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the 'Software'), to deal in the Software without
+ * restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so,subject to
+ * the following conditions:
+ * The above copyright notice and this permission notice shall
+ * be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED ''AS IS'', WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+/**
+ * @file Node.h
+ * @brief  A structure to denote each block in map
+ * @author Bhargav Dandamudi
+ * @version 1
+ * @date 2019-04-03
+ */
 #pragma once
 #include <iostream>
 #include <utility>
 
+/* ----------------------------------------------------------------*/
+/**
+ * @brief  Basic Node Structure for each block
+ */
+/* ----------------------------------------------------------------*/
 struct Node {
-  Node();
-  Node(int, std::pair<int, int>);
+  Node();                         // Default constructor
+  Node(int, std::pair<int, int>); // second constructor to initialise with
+                                  // data members
   std::pair<int, int> position_;
-  // Node *parent_;
-  int value_;
+  int value_; // this tells if its obstacle(1) or free space (0)
 };
--- include/RandomPlanner.h
+++ include/RandomPlanner.h
@@ -1,3 +1,34 @@
+/* Copyright (C)
+ * 2019 - Bhargav Dandamudi
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the 'Software'), to deal in the Software without
+ * restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so,subject to
+ * the following conditions:
+ * The above copyright notice and this permission notice shall
+ * be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED ''AS IS'', WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH
+ * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+/**
+ * @file RandomPlanner.h
+ * @brief  Main Random Discrete Planner Class with all declarations
+ * @author Bhargav Dandamudi
+ * @version 1
+ * @date 2019-04-03
+ */
 #pragma once
 #include "./Node.h"
 #include <algorithm>
@@ -7,10 +38,21 @@
 #include <time.h>
 #include <utility>
 #include <vector>
+/* ----------------------------------------------------------------*/
+/**
+ * @brief The random planner tries to find a path to the goal by randomly moving
+in the environment (only orthogonal moves are legal). If the planner can not
+find an acceptable solution in less than max_step_number, the search should
+fail. The random planner, while being erratic, has a short memory, and it will
+never attempt to visit a cell that was visited in the last sqrt(max_step_number)
+steps except if this is the only available option.
+ */
+/* ----------------------------------------------------------------*/
 class RandomPlanner {
 private:
-  int max_step_number = 100;
-  std::deque<std::pair<int, int>> last_steps;
+  int max_step_number = 100; // as in problem statement, can be any +ve integer
+  std::deque<std::pair<int, int>>
+      last_steps; // Queue Memory of steps taken so far
   std::vector<std::vector<int>> world_map;
   std::pair<int, int> robot_pose;
   std::pair<int, int> goal_pose;
@@ -18,28 +60,122 @@ private:
   Node start_node;
   Node goal_node;
   Node current_node;
-  int dir;
-  int y_length;
-  int x_length;
+  int dir;      // direction inwhich robot will move
+  int y_length; // max height of map in y direction
+  int x_length; // max width of map in x direction
 
 public:
+  /* ----------------------------------------------------------------*/
+  /**
+   * @brief Default constructor to take map, starting position and goal
+   * position
+   * of robot
+   * @param std::vector<std::vector> world map
+   * @param std::pair starting position
+   * @param std::pair goal position
+   */
+  /* ----------------------------------------------------------------*/
   RandomPlanner(std::vector<std::vector<int>>, std::pair<int, int>,
                 std::pair<int, int>);
-  //  void setYLength();
-  // void setXLength();
+  /* ----------------------------------------------------------------*/
+  /**
+   * @brief  to Move in up direction, updates current node
+   *
+   * @param Node present node location
+   *
+   * @return Update Node to move in upper block
+   */
+  /* ----------------------------------------------------------------*/
   Node moveUp(Node);
+  /* ----------------------------------------------------------------*/
+  /**
+   * @brief  TO move in left location
+   *
+   * @param Node node to be moved
+   *
+   * @return updated Node
+   */
+  /* ----------------------------------------------------------------*/
   Node moveLeft(Node);
+  /* ----------------------------------------------------------------*/
+  /**
+   * @brief  TO move in +y direction
+   *
+   * @param Node node to be udpated
+   *
+   * @return updated Node
+   */
+  /* ----------------------------------------------------------------*/
   Node moveDown(Node);
+  /* ----------------------------------------------------------------*/
+  /**
+   * @brief   to move in +x direction
+   *
+   * @param Node node to be updated
+   *
+   * @return updated node
+   */
+  /* ----------------------------------------------------------------*/
   Node moveRight(Node);
-  void setGoalNode();
-  void setStartNode();
+  /* ----------------------------------------------------------------*/
+  /**
+   * @brief to fetch goal node location
+   */
+  /* ----------------------------------------------------------------*/
   void getGoal();
+  /* ----------------------------------------------------------------*/
+  /**
+   * @brief  to get current pose of the robot
+   */
+  /* ----------------------------------------------------------------*/
   void getPose();
+  /* ----------------------------------------------------------------*/
+  /**
+   * @brief  To check if robot is in defined boundary
+   *
+   * @param std::pair robot coordinates
+   *
+   * @return true if its blocked/obstacle, else false
+   */
+  /* ----------------------------------------------------------------*/
   bool isObstacle(std::pair<int, int>);
+  /* ----------------------------------------------------------------*/
+  /**
+   * @brief  to give random direction everytime we run this function
+   *          in range of 1 to 4
+   * @return random direction as {1,2,3,4}
+   */
+  /* ----------------------------------------------------------------*/
   int randomDirection();
+  /* ----------------------------------------------------------------*/
+  /**
+   * @brief  update the last steps queue to maintain memory
+   *
+   * @param Node location of new location to be updated in memory
+   */
+  /* ----------------------------------------------------------------*/
   void updateLastSteps(Node);
+  /* ----------------------------------------------------------------*/
+  /**
+   * @brief  To check if input node is in memory or not
+   *
+   * @param Node
+   *
+   * @return true if the node is in memory
+   */
+  /* ----------------------------------------------------------------*/
   bool checkLastNSteps(Node);
-  bool canWeGo(Node);
+  /* ----------------------------------------------------------------*/
+  /**
+   * @brief  to print the robot path if found
+   */
+  /* ----------------------------------------------------------------*/
   void printPath();
+  /* ----------------------------------------------------------------*/
+  /**
+   * @brief  To move the robot randomly as per given direction until
+   *          max number of steps are out or goal is reached.
+   */
+  /* ----------------------------------------------------------------*/
   void moveAround();
 };
